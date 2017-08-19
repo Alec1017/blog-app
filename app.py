b0eee179f65622da1b422888907ae40f06c51360
Alec DiFederico <alecdifederico@gmail.com>
2017-08-19 17:23:31 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+# Create instance of flask class.
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
