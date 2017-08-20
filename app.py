46908f345628cd499450f449fc775c3f459bc222
Alec DiFederico <alecdifederico@gmail.com>
2017-08-19 21:00:43 -0400

    added article list

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from data import Blog_posts
 
 # Create instance of flask class.
 app = Flask(__name__)
+
+# The dummy articles from data.py
+blog_posts = Blog_posts()
 
 
 # Renders the home page
@@ -14,6 +18,18 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+# Renders the articles page
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=blog_posts)
+
+
+# Renders a specific article
+@app.route('/article/<string:id>/')
+def get_article(id):
+    return render_template('article.html', id=id)
 
 
 # Run the application
---
+++ data.py
@@ -0,0 +1,27 @@
+# Dummy article data used as a placeholder for now
+def Blog_posts():
+    blog_posts = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'John Smith',
+            'create_date': '08-19-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Matt Johnson',
+            'create_date': '12-25-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Jane Doe',
+            'create_date': '04-21-2016'
+        }
+    ]
+
+    return blog_posts
